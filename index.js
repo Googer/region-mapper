31fee1b7822f3f6bbae6431bb49c8a4744706083
Googer <Googer@users.noreply.github.com>
2017-09-21 12:07:25 -0400

    Use turf instead of d3-geo for determining this gym region mapping; gives flexibility to manipulate regions (grow, etc.) if desired

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
 "use strict";
 
-const D3 = require('d3-geo'),
-	ToGeoJSON = require('togeojson-with-extended-style'),
-	fs = require('fs'),
-	DOMParser = require('xmldom').DOMParser,
-	kml = new DOMParser().parseFromString(fs.readFileSync('map.kml', 'utf8')),
-	regions = ToGeoJSON.kml(kml).features,
-	regionMap = Object.create(null);
+const ToGeoJSON = require('togeojson-with-extended-style'),
+  fs = require('fs'),
+  turfHelpers = require('@turf/helpers'),
+  turfInside = require('@turf/inside'),
+  turfScale = require('@turf/transform-scale'),
+  DOMParser = require('xmldom').DOMParser,
+  kml = new DOMParser().parseFromString(fs.readFileSync('map.kml', 'utf8')),
+  rawRegions = ToGeoJSON.kml(kml).features,
+  regionMap = Object.create(null);
 
-regions.forEach(region => {
-	region.geometry.coordinates[0].reverse();
+// flip order of coordinates so they're in the right order according to what D3 expects
+rawRegions.forEach(region => {
+  region.geometry.coordinates[0].reverse();
+//  region.geometry = turfScale(region.geometry, 1.1);
 });
 
-const d3Regions = regions
-		.map(feature => [feature.properties.name, feature.geometry]),
-	gyms = require('./gyms')
-		.map(gym => [gym.gymId, [gym.gymInfo.longitude, gym.gymInfo.latitude]]);
+const mapRegions = rawRegions
+    .map(feature => [feature.properties.name, feature.geometry]),
+  gyms = require('./gyms')
+    .map(gym => [gym.gymId, turfHelpers.point([gym.gymInfo.longitude, gym.gymInfo.latitude])]);
 
+let output = '';
+require('./gyms').forEach(gym => {
+  output = output + gym.gymName + '\t' + gym.gymInfo.longitude + '\t' + gym.gymInfo.latitude + '\n';
+});
+
+fs.writeFileSync('gyms.tsv', output);
 
 gyms.forEach(gym => {
-	const matchingRegions = d3Regions.filter(region => D3.geoContains(region[1], gym[1]));
+  const matchingRegions = mapRegions.filter(region => turfInside(gym[1], region[1]));
 
-	let regionGyms;
-	matchingRegions.forEach(matchingRegion => {
-		regionGyms = regionMap[matchingRegion[0]];
+  let regionGyms;
+  matchingRegions.forEach(matchingRegion => {
+    regionGyms = regionMap[matchingRegion[0]];
 
-		if (!regionGyms) {
-			regionGyms = [];
-			regionMap[matchingRegion[0]] = regionGyms;
-		}
+    if (!regionGyms) {
+      regionGyms = [];
+      regionMap[matchingRegion[0]] = regionGyms;
+    }
 
-		regionGyms.push(gym[0]);
-	});
+    regionGyms.push(gym[0]);
+  });
 });
 
 fs.writeFileSync('region-map.json', JSON.stringify(regionMap, null, 2));
